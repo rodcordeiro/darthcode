f62a62ffccc9f8a571e5710555e02f5d1ee8ccae
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-05-22 22:41:11 -0300

    first commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './index.css'
+import SithEmblem from '../../assets/SithEmblemTOR.png';
+
+function MainPage() {
+  return (
+    <div className="container">
+      <header>
+        <img className="logo" src={SithEmblem} alt="SithEmblem" />
+        <nav>
+          <ul>
+            <li><Link className="menuLink active" to="/" onClick={()=>{}}>Home</Link></li>
+            <li><Link className="menuLink" to="/" onClick={()=>{}}>Projects</Link></li>
+            <li><Link className="menuLink" to="/" onClick={()=>{}}>About Us</Link></li>
+            <li><Link className="menuLink" to="/" onClick={()=>{}}>Contact</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <div className="content">
+        <h1>DARTH CODE</h1>
+        <p>
+          Inspired by the force, we are Darth Coders that are building the world.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import MainPage from './pages/Main';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+          <Switch>
+              <Route path="/" exact component={MainPage}/>
+          </Switch>
+        </BrowserRouter>
+    );
+}
